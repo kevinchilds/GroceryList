3b243cb2ffd404816cb63caba6e3b2ac3a049acf
Kevin Desktop <kchilds2020@gmail.com>
2020-04-25 13:45:23 -0500

    adding mongodb inside of express

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const path = require('path');
 
+const dbName = "Portfolio";
+const colName = "groceryList";
+var assert = require('assert');
+var MongoClient = require('mongodb').MongoClient;
+
+
 const app = express();
 
 app.use(express.static(path.join(__dirname,'../Client')));
@@ -8,3 +14,25 @@ app.use(express.static(path.join(__dirname,'../Client')));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {console.log(`Server started on ${PORT}`);});
+
+
+
+MongoClient.connect("mongodb+srv://kchilds2020:Gertie2018@redesignforme-9mmku.azure.mongodb.net", {useUnifiedTopology: true} ,function(err, client) {
+  if(err) { return console.dir(err);}
+  const db = client.db(dbName);
+  console.log(`connected to \'${dbName}\' database and \'${colName}\' collection`);
+
+  /* db.collection(colName, function(err, collection){
+        collection.find().pretty();
+  }); */
+ app.get('/list', (req, res) => {
+     let data = db.collection('groceryList').find().toArray()
+     .then(results => {
+         console.log(results)
+         res.json(results);
+     })
+     .catch(error => console.error(error))
+     
+ })
+
+});
