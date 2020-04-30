02eb448b0df5f9c9811c87d774a79f6616131790
Kevin Desktop <kchilds2020@gmail.com>
2020-04-30 02:29:15 -0500

    testing

--- Server/index.js
+++ Server/index.js
@@ -8,16 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 
 //location of frontend
-//app.use(express.static(path.join(__dirname,'../Client')));
-
-/* if(process.env.NODE_ENV === 'production'){
-    //set static folder
-    app.use(express.static('../Client'));
-} */
-app.use(express.static('../Client'));
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, '../Client', 'index.html'));
-});
+app.use(express.static(path.join(__dirname,'../Client')));
 
 
 //location of APIs
