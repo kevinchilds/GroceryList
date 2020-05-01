67415b9379c08e5aa2d9bcacc13f2c515ca01360
kchilds2020 <63356648+kchilds2020@users.noreply.github.com>
2020-05-01 16:18:13 -0500

    testing mongodb

--- Server/apis/groceryList.js
+++ Server/apis/groceryList.js
@@ -7,31 +7,35 @@ var assert = require('assert');
 
 
 MongoClient.connect('mongodb+srv://GroceryList:DbwflWOmW9Qagh4f@redesignforme-9mmku.azure.mongodb.net', {useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true } ,function(err, client) {
-    if(err) { return console.dir(err);}
-    const db = client.db(dbName);
-  
-    //return groceryList document if ID exists
-    router.get('/grocery-list/:id', (req, res) => {
-        let data = db.collection('groceryList').find({ listkey: req.params.id}).toArray()
-        .then(results => {
-            console.log(results);
-            res.json(results);
+    if(err) { 
+        return console.log('Unable to connect to mongo server: ', err);
+    }
+    else{
+        console.log('MONGO DB HAS SUCCESSFULLY CONNECTED');
+        const db = client.db(dbName);
+    
+        //return groceryList document if ID exists
+        router.get('/grocery-list/:id', (req, res) => {
+            let data = db.collection('groceryList').find({ listkey: req.params.id}).toArray()
+            .then(results => {
+                console.log(results);
+                res.json(results);
+            })
+            .catch(error => console.error(error))    
         })
-        .catch(error => console.error(error))    
-    })
 
-    //adds a new groceryList document
-    router.post('/add-list', (req, res) => {
-        let data = db.collection('groceryList').insert({
-            listkey: req.body.listkey
+        //adds a new groceryList document
+        router.post('/add-list', (req, res) => {
+            let data = db.collection('groceryList').insert({
+                listkey: req.body.listkey
+            })
+            .then(results => {
+                console.log(results);
+                res.json(results);
+            })
+            .catch(error => console.error(error))
         })
-        .then(results => {
-            console.log(results);
-            res.json(results);
-        })
-        .catch(error => console.error(error))
-    })
-
+    }
 });
 
 
--- Server/apis/item.js
+++ Server/apis/item.js
@@ -7,95 +7,99 @@ var assert = require('assert');
 
 
 MongoClient.connect('mongodb+srv://GroceryList:DbwflWOmW9Qagh4f@redesignforme-9mmku.azure.mongodb.net', {useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true } ,function(err, client) {
-    if(err) { return console.dir(err);}
-    const db = client.db(dbName);
+    if(err) { 
+        return console.log('Failed connecting to server: ', err);
+    }else{
+        const db = client.db(dbName);
 
-    //return all items in a grocery list
-    router.get('/all-items/:id', (req, res) => {
-        let data = db.collection('item').find({listkey: req.params.id}).toArray()
+        //return all items in a grocery list
+        router.get('/all-items/:id', (req, res) => {
+            let data = db.collection('item').find({listkey: req.params.id}).toArray()
+            .then(results => {
+                console.log(results)
+                res.json(results);
+            })
+            .catch(error => console.error(error))    
+        })
+
+        //verify there is incart items
+        router.get('/validate-cart/:id', (req, res) => {
+        let data = db.collection('item').find({ listkey: req.params.id, inCart: true}).toArray()
+        .then(results => {
+            console.log(results);
+            res.json(results);
+        })
+        .catch(error => console.error(error))    
+        })
+
+        //return a specific item from a specific list
+        router.get('/:item/:id', (req, res) => {
+        let data = db.collection('item').find({ listkey: req.params.id, text: req.params.item}).toArray()
+        .then(results => {
+            console.log(results);
+            res.json(results);
+        })
+        .catch(error => console.error(error))    
+        })
+
+        //update inCart value
+        router.post('/toggle-cart/:id', (req, res) => {
+        let data = db.collection('item').update({ text: req.body.text, listkey: req.params.id},
+            {
+                $set: {
+                    inCart: req.body.inCart
+                }
+            }     
+        ) 
+        .then(results => {
+            console.log(results);
+            res.json(results);
+        })
+        .catch(error => console.error(error))
+        })
+
+        //add item to specific grocery list
+        router.post('/add-item', (req, res) => {
+        let data = db.collection('item').insert({
+            text: req.body.text,
+            inCart: req.body.inCart,
+            listkey: req.body.listkey
+        })
+        .then(results => {
+            console.log(results);
+            res.json(results);
+        })
+        .catch(error => console.error(error))
+        })
+
+
+        //remove item from specific grocery list
+        router.post('/remove-item', (req, res) => {
+        let data = db.collection('item').remove({
+            text: req.body.text,
+            listkey: req.body.listkey
+        })
+        .then(results => {
+            console.log(results);
+            res.json(results);
+        })
+        .catch(error => console.error(error))
+        })
+
+        //remove all items with inCart = true from specific grocery list
+        router.post('/remove-incart-items/:id', (req, res) => {
+        let data = db.collection('item').remove({
+            listkey: req.params.id,
+            inCart: true
+        })
         .then(results => {
             console.log(results)
             res.json(results);
         })
         .catch(error => console.error(error))    
-    })
+        })
 
-    //verify there is incart items
-    router.get('/validate-cart/:id', (req, res) => {
-    let data = db.collection('item').find({ listkey: req.params.id, inCart: true}).toArray()
-    .then(results => {
-        console.log(results);
-        res.json(results);
-    })
-    .catch(error => console.error(error))    
-    })
-
-    //return a specific item from a specific list
-    router.get('/:item/:id', (req, res) => {
-    let data = db.collection('item').find({ listkey: req.params.id, text: req.params.item}).toArray()
-    .then(results => {
-        console.log(results);
-        res.json(results);
-    })
-    .catch(error => console.error(error))    
-    })
-
-    //update inCart value
-    router.post('/toggle-cart/:id', (req, res) => {
-    let data = db.collection('item').update({ text: req.body.text, listkey: req.params.id},
-        {
-            $set: {
-                inCart: req.body.inCart
-            }
-        }     
-    ) 
-    .then(results => {
-        console.log(results);
-        res.json(results);
-    })
-    .catch(error => console.error(error))
-    })
-
-    //add item to specific grocery list
-    router.post('/add-item', (req, res) => {
-    let data = db.collection('item').insert({
-        text: req.body.text,
-        inCart: req.body.inCart,
-        listkey: req.body.listkey
-    })
-    .then(results => {
-        console.log(results);
-        res.json(results);
-    })
-    .catch(error => console.error(error))
-    })
-
-
-    //remove item from specific grocery list
-    router.post('/remove-item', (req, res) => {
-    let data = db.collection('item').remove({
-        text: req.body.text,
-        listkey: req.body.listkey
-    })
-    .then(results => {
-        console.log(results);
-        res.json(results);
-    })
-    .catch(error => console.error(error))
-    })
-
-    //remove all items with inCart = true from specific grocery list
-    router.post('/remove-incart-items/:id', (req, res) => {
-    let data = db.collection('item').remove({
-        listkey: req.params.id,
-        inCart: true
-    })
-    .then(results => {
-        console.log(results)
-        res.json(results);
-    })
-    .catch(error => console.error(error))    
-    })
+    }
 
 });
 
