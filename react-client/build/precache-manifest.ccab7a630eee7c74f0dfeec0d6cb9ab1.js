940d6aa7f4a404c5a8905bf7f70f5ccda250cba3
kchilds2020 <63356648+kchilds2020@users.noreply.github.com>
2020-07-30 16:12:02 -0700

    testing

---
+++ react-client/build/precache-manifest.ccab7a630eee7c74f0dfeec0d6cb9ab1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f1f9263e9bdf475b25a8fe75278cd14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b21a85b13f384bea5998",
+    "url": "/static/css/main.2c8c6098.chunk.css"
+  },
+  {
+    "revision": "65887c9eb7c5fa79ac5e",
+    "url": "/static/js/2.b0b91a06.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b0b91a06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b21a85b13f384bea5998",
+    "url": "/static/js/main.175f6c5f.chunk.js"
+  },
+  {
+    "revision": "0dc9215c4b0a17c8c874",
+    "url": "/static/js/runtime-main.7bf3d854.js"
+  }
+]);
